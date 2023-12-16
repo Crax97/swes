33ba2cb3c91e5452f8850fef56f2dff3a8e6d86a
Crax97 <gsolimeno97@gmail.com>
2023-12-16 22:34:48 +0100

    Move blog entry to separate module

---
+++ src/blog_storage.rs
@@ -0,0 +1,45 @@
+use std::path::{Path, PathBuf};
+
+use serde::{Deserialize, Serialize};
+use yaml_front_matter::YamlFrontMatter;
+
+#[derive(Serialize, Deserialize)]
+pub struct PostMetadata {
+    pub title: String,
+}
+
+pub struct BlogEntry {
+    pub description: PostMetadata,
+    pub html: String,
+}
+
+pub struct BlogStorage {
+    base_path: PathBuf,
+}
+
+impl BlogStorage {
+    pub fn new<P: AsRef<Path>>(base: P) -> Self {
+        Self {
+            base_path: PathBuf::from(base.as_ref()),
+        }
+    }
+
+    pub fn get_entry(&self, entry_name: &str) -> anyhow::Result<BlogEntry> {
+        Self::parse_file_to_html(&self.base_path.join(entry_name))
+    }
+    fn parse_file_to_html<P: AsRef<Path>>(path: &P) -> anyhow::Result<BlogEntry> {
+        let content = std::fs::read_to_string(path)?;
+        let document = YamlFrontMatter::parse::<PostMetadata>(&content);
+        let document = match document {
+            Ok(doc) => doc,
+            Err(e) => {
+                anyhow::bail!(e.to_string())
+            }
+        };
+        let html = comrak::markdown_to_html(&document.content, &comrak::Options::default());
+        Ok(BlogEntry {
+            description: document.metadata,
+            html,
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,21 @@
-use serde::{Deserialize, Serialize};
-use std::path::Path;
+mod blog_storage;
 
 use clap::Parser;
-use yaml_front_matter::YamlFrontMatter;
+
+use crate::blog_storage::BlogStorage;
 
 #[derive(Parser, Debug)]
 struct Args {
     #[arg(short, long)]
     file: String,
-}
-
-#[derive(Serialize, Deserialize)]
-struct Frontmatter {
-    title: String,
-}
-
-struct ParsedDocument {
-    description: Frontmatter,
-    html: String,
+    #[arg(short, long)]
+    base_path: Option<String>,
 }
 
 fn main() {
     let args = Args::parse();
-    let file = parse_file_to_html(&args.file).unwrap();
-    println!("Title: {}", file.description.title);
-    println!("Content: \n{}", file.html);
-}
-
-fn parse_file_to_html<P: AsRef<Path>>(
-    path: &P,
-) -> Result<ParsedDocument, Box<dyn std::error::Error>> {
-    let content = std::fs::read_to_string(path)?;
-    let document = YamlFrontMatter::parse::<Frontmatter>(&content)?;
-    let html = comrak::markdown_to_html(&document.content, &comrak::Options::default());
-    Ok(ParsedDocument {
-        description: document.metadata,
-        html,
-    })
+    let storage = BlogStorage::new(args.base_path.unwrap_or("blog".to_owned()));
+    let post = storage.get_entry(&args.file).unwrap();
+    println!("Title: {}", post.description.title);
+    println!("Content: \n{}", post.html);
 }
